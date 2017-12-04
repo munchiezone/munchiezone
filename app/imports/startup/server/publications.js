61230756566212e5e30262c0996b6a9c777059f3
WendyChen9 <31325518+WendyChen9@users.noreply.github.com>
2017-12-03 14:42:51 -1000

    fixed Order Collection

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -1,8 +1,10 @@
 import { Restaurants } from '/imports/api/restaurant/RestaurantCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Orders } from '/imports/api/order/OrderCollection';
 
 
 Restaurants.publish();
 Interests.publish();
 Profiles.publish();
+Orders.publish();
